6bf569d1a93293068039bfd08f6e57a6922c904a
Dmitry <karpov0910@mail.ru>
2024-03-15 06:54:02 +0300

    Продюсер и консюмер добавлены как модули в один проект

---
+++ KafkaMicroservice/ConsumerMicroservice/src/test/java/com/example/stubConsumer/StubConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stubConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StubConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
